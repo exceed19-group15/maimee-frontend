eb8ad7f5953da726b7130a14c96b19a05d697b4a
breezjirasak <jirasak.tu@ku.th>
2023-02-12 10:47:22 +0700

    improve UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import Home from "./pages/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Play from "./pages/Play.jsx";
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Maimee</title>
+      </Helmet>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Board from "../components/Board";
 import { getBeats } from "../services/Maimee";
 import { getLeaderBoard } from "../services/Maimee";
 import { update_Gamestate } from "../services/Mai";
+import logo from "../assets/Maimee1.png"
 
 
 const Home = () => {
@@ -24,15 +25,20 @@ const Home = () => {
       update_Gamestate({"game_state": "MENU", "beatmap_id": null})
   },[number])
 
+  console.log(records)
+
   return (
     <div>
       <div className="col-1">
-        <p>BEAT</p>
+        <img src={logo} alt="" />
+        <p>Choose Your Beat To Play The Game!!!</p>
+
         {number === -1 ? null : <h1>LEADERBOARD</h1>}
+        {number !== -1 && records.length === 0 ? <h3>There is no record</h3> : null}
         {number === -1
           ? null
           : records
-              .map((record) => <Board {...record} />)}
+              .map((record, index) => <Board {...record} />)}
       </div>
       <div className="col-2">
         {beats.map((beat) => (
